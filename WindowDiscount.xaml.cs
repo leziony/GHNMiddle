210473bfbdbbdb55b6945ec02a857413fc7e5f93
leziony <Leziony@DESKTOP-KNBVON7>
2023-07-25 09:28:17 +0200

    Discount window

---
+++ WindowDiscount.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GHNMiddle
+{
+    /// <summary>
+    /// Logika interakcji dla klasy WindowDiscount.xaml
+    /// </summary>
+    public partial class WindowDiscount : Window
+    {
+        public WindowDiscount()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
