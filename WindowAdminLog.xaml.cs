7826ce6b0ac82262a85c26cc403d8b98c38540c4
leziony <Leziony@DESKTOP-KNBVON7>
2023-07-27 15:06:31 +0200

    New Window

---
+++ WindowAdminLog.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GHNMiddle
+{
+    /// <summary>
+    /// Interaction logic for WindowAdminLog.xaml
+    /// </summary>
+    public partial class WindowAdminLog : Window
+    {
+        public WindowAdminLog()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var test = MessageBox.Show("Funkcja w testach. \n Czy chcesz sprawdzić czy to działa?","Testy",MessageBoxButton.YesNo,MessageBoxImage.Question);
+            if (test == MessageBoxResult.Yes)
+            {
+                MessageBox.Show(login.Text + " " + password.Text,"Testy");
+            }
+           
+            this.Close();
+        }
+    }
+}
