cf93b9d8318c936dac712f004edaad5ded36c9be
leziony <Leziony@DESKTOP-KNBVON7>
2023-07-28 08:51:41 +0200

    New window implemenation

---
+++ WindowAdmin.xaml.cs
@@ -0,0 +1,57 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GHNMiddle
+{
+    /// <summary>
+    /// Interaction logic for WindowAdmin.xaml
+    /// </summary>
+    public partial class WindowAdmin : Window
+    {
+        MainWindow conn = new MainWindow();
+        string currentBase;
+        public void DatabasePull()
+        {
+            conn.connectsql("server=localhost;uid=root;pwd=admin;database=ghndata;");
+            conn.conn.Open();
+            string sql = "SHOW TABLES IN ghndata";
+            MySqlCommand cmd = new MySqlCommand(sql,conn.conn);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                selectDatabase.Items.Add(dr.GetString(0));
+            }
+            conn.conn.Close();
+            currentBase = selectDatabase.SelectedItem.ToString();
+            
+        }
+        public WindowAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            currentBase = selectDatabase.SelectedItem.ToString();
+        }
+
+        private void WindowAdmin_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            conn.conn.Dispose();
+            conn.Close();
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
