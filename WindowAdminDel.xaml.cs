70de492bf33fa93a978c81392f0eeafeb97180a9
leziony <Leziony@DESKTOP-KNBVON7>
2023-08-02 11:06:34 +0200

    New Window

---
+++ WindowAdminDel.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GHNMiddle
+{
+    /// <summary>
+    /// Interaction logic for WindowAdminDel.xaml
+    /// </summary>
+    public partial class WindowAdminDel : Window
+    {
+        public WindowAdminDel()
+        {
+            InitializeComponent();
+        }
+    }
+}
