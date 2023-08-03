f3c5534a700dd88aa92cb3cf08cf60ef8184e06b
leziony <Leziony@DESKTOP-KNBVON7>
2023-08-03 11:09:37 +0200

    New window

---
+++ WindowHDL24.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GHNMiddle
+{
+    /// <summary>
+    /// Interaction logic for WindowHDL24.xaml
+    /// </summary>
+    public partial class WindowHDL24 : Window
+    {
+        public WindowHDL24()
+        {
+            InitializeComponent();
+        }
+    }
+}
