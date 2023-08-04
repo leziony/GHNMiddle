b1cf317adf83eaf8d287884a110f6a5400a24510
leziony <Leziony@DESKTOP-KNBVON7>
2023-08-04 13:23:28 +0200

    Login applied

--- WindowID.xaml.cs
+++ WindowID.xaml.cs
@@ -31,26 +31,56 @@ namespace GHNMiddle
         {
             wincon.connectsql("server=localhost;uid=root;pwd=admin;database=ghndata;");
             wincon.conn.Open();
-            string sql = "CREATE TABLE IF NOT EXISTS "+ UserID.Text +"(" +
-                "tarrifcode varchar(255)," +
-                "ammount int," +
-                "unit varchar(255)," +
-                "cost decimal(15,2)" +
-                ");";
+            string sql;
+            sql = "SELECT * FROM users WHERE nazwisko = ?t1 AND password = ?t2 LIMIT 1";
             MySqlCommand cmd = new MySqlCommand(sql, wincon.conn);
-            cmd.ExecuteNonQuery();
-            wincon.changeId(UserID.Text);
-            WindowGA Test = new WindowGA(UserID.Text);
-            Test.Show();
-            complete = true;
-            wincon.conn.Dispose();
-            wincon.Close();
-            this.Close();
+            cmd.Parameters.Add(new MySqlParameter("t1", UserID.Text));
+            cmd.Parameters.Add(new MySqlParameter("t2",password.Text));
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                rdr.Read();
+                string pass = rdr["password"].ToString();
+                if (pass == password.Text)
+                {
+                    int id = (int)rdr["ID"];
+                    wincon.conn.Close();
+                    wincon.conn.Open();
+                    sql = "CREATE TABLE IF NOT EXISTS " + UserID.Text + "temp (" +
+                    "tarrifcode varchar(255)," +
+                    "ammount int," +
+                    "unit varchar(255)," +
+                    "cost decimal(15,2)" +
+                    ");";
+                    cmd = new MySqlCommand(sql, wincon.conn);
+                    cmd.ExecuteNonQuery();
+                    wincon.changeId(UserID.Text);
+                    WindowGA Test = new WindowGA(UserID.Text);
+                    Test.Show();
+                    complete = true;
+                    wincon.conn.Dispose();
+                    wincon.Close();
+                    this.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("Error at login!","Error");
+                    wincon.conn.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error at login!", "Error");
+                wincon.conn.Close();
+            }
+          
 
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            wincon.conn.Dispose();
             wincon.Close();
             if(complete == false) { Application.Current.MainWindow.Show(); }
         }
